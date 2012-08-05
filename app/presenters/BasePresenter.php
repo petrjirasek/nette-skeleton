ce0ff4a6406fc384ce7d8295e013d7b954cdae26
Jirda <mail@petrjirasek.cz>
2012-08-05 17:18:42 +0200

    Added abd implemented webloader

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -6,4 +6,43 @@
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+    protected function createComponentCss()
+    {
+        $files = new WebLoader\FileCollection(WWW_DIR . '/css');
+        $files->addFiles(array(
+            WWW_DIR . '/bootstrap/css/bootstrap.css',
+            WWW_DIR . '/bootstrap/css/responsive.css',
+            'styles.css',
+        ));
+
+        $compiler = WebLoader\Compiler::createCssCompiler($files, WWW_DIR . '/temp');
+/*        $compiler->addFilter(function ($code) {
+            return cssmin::minify($code, "remove-last-semicolon");
+        });*/
+
+        $control = new WebLoader\Nette\CssLoader($compiler, $this->template->basePath . '/temp');
+        $control->setMedia('screen');
+
+        return $control;
+    }
+
+
+    protected function createComponentJs()
+    {
+        $files = new WebLoader\FileCollection(WWW_DIR . '/js');
+        $files->addRemoteFile('http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js');
+        $files->addFiles(array(
+            WWW_DIR . '/bootstrap/js/bootstrap.min.js',
+            'netteForms.js',
+        ));
+
+        $compiler = WebLoader\Compiler::createJsCompiler($files, WWW_DIR . '/temp');
+        /*        $compiler->addFilter(function ($code) {
+            return cssmin::minify($code, "remove-last-semicolon");
+        });*/
+
+        $control = new WebLoader\Nette\JavaScriptLoader($compiler, $this->template->basePath . '/temp');
+
+        return $control;
+    }
 }
